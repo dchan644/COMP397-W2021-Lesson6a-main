7cad817cbdc3c7b5592fcae7033eca080fb12a26
Derek <d.chan644@gmail.com>
2021-02-27 23:55:30 -0500

    Added LOS and chase

---
+++ Assets/LuchaBehavior.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public enum LuchaState
+{
+    IDLE,
+    RUN,
+    JUMP
+}
+public class LuchaBehavior : MonoBehaviour
+{
+    [Header("Line of Sight")]
+
+    public bool HasLOS;
+    
+    public GameObject player;
+
+    private NavMeshAgent agent;
+    private Animator animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (HasLOS)
+        {
+            agent.SetDestination(player.transform.position);
+            animator.SetInteger("AnimState", (int)LuchaState.RUN);
+            if(Vector3.Distance(transform.position, player.transform.position)< 2.5)
+            {
+                //attack goes here
+                animator.SetInteger("AnimState", (int)LuchaState.IDLE);
+                transform.LookAt(transform.position - player.transform.forward);
+            }
+            else
+            {
+                animator.SetInteger("AnimState", (int)LuchaState.RUN);
+            }
+        }
+        else
+        {
+            animator.SetInteger("AnimState", (int)LuchaState.IDLE);
+        }
+
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            animator.SetInteger("AnimState", (int)LuchaState.IDLE);
+        }
+        if (Input.GetKeyDown(KeyCode.U))
+        {
+            animator.SetInteger("AnimState", (int)LuchaState.RUN);
+        }
+
+        if (Input.GetKeyDown(KeyCode.J))
+        {
+            animator.SetInteger("AnimState", (int)LuchaState.JUMP);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+
+            HasLOS = true;
+            player = other.transform.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            
+            HasLOS = false;
+        }
+    }
+
+
+}
